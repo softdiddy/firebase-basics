d91cb29aa7b2517f9cc5e6330ea361b1d0e58107
Mohammed Lawal Abubakar <softdiddy@gmail.com>
2022-03-23 10:35:36 -0700

    firebase config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("Go ");
+import {initializeApp} from 'firebase/app'
+import {
+    getFirestore,collection,getDocs
+}from 'firebase/firestore'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDd7FIkCcjhF2AOpMso1mI-fe__JowxgoM",
+    authDomain: "fir-9-basics-f51ba.firebaseapp.com",
+    projectId: "fir-9-basics-f51ba",
+    storageBucket: "fir-9-basics-f51ba.appspot.com",
+    messagingSenderId: "675783802071",
+    appId: "1:675783802071:web:5e862e0d8980c840e2c492",
+    measurementId: "G-SM39D828N6"
+  };
+
+  //initializing the app
+  initializeApp(firebaseConfig);
+
+  //init services
+  const db=getFirestore()
+
+  //collection ref
+  const colRef=collection(db,'books')
+
+  
